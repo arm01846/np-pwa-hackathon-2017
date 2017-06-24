35694ba82aae669e4f1fdbe2e29a1ad969668485
Sukrit Amornpornwiwat <arm01846@gmail.com>
2017-06-25 01:40:31 +0700

    add spinner

--- src/routes/menu/index.js
+++ src/routes/menu/index.js
@@ -1,7 +1,7 @@
 import { h, Component } from 'preact';
 import style from './style';
 import mdl from 'material-design-lite/material';
-import { List, ListItem } from 'preact-mdl';
+import { List, ListItem, Spinner } from 'preact-mdl';
 import MenuItem from '../../components/menu-item';
 
 export default class Menu extends Component {
@@ -22,13 +22,18 @@ export default class Menu extends Component {
 	render({}, { menu }) {
 		return (
 			<div class={style.menu}>
-				<List style={{ margin: '0' }}>	
-				{ menu.map((coffee) => {
-					return <ListItem three-line class={style.listItem} >
-						<MenuItem coffee={coffee}></MenuItem>
-					</ListItem>
-				}) }
-				</List>
+				{ !menu || menu.length === 0 ? (
+					<Spinner active={true}
+						class={style.spinner}/>
+				):(
+					<List style={{ margin: '0' }}>	
+					{ menu.map((coffee) => {
+						return <ListItem three-line class={style.listItem} >
+							<MenuItem coffee={coffee}></MenuItem>
+						</ListItem>
+					}) }
+					</List>
+				)}
 			</div>
 		);
 	}
