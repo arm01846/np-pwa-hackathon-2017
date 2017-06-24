dc47269b02073bff00562952b82e31d843d4c48c
Natkrita Chumlek <sariakra@gmail.com>
2017-06-24 19:07:30 +0700

    add route to detail page

--- src/routes/detail/index.js
+++ src/routes/detail/index.js
@@ -2,45 +2,84 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Detail extends Component {
-	state = {
-		time: Date.now(),
-		count: 10
-	};
+    state = {
+        name: "เอสเปรสโซ่",
+        image: "https://globalassets.starbucks.com/assets/b0525676a0194652be7f4993210b381a.jpg",
+        ingredients: ["กาแฟ"],
+        size: [
+            {
+                name: "Solo",
+                volume: 1,
+                variations: [
+                    {
+                        name: "",
+                        nutrition: {
+                            calories: 6.3,
+                            fat: 0,
+                            sugar: 0,
+                            protein: 0.5,
+                            caffeine: 93.8
+                        }
+                    }
+                ]
+            },
+            {
+                name: "Doppio",
+                volume: 2,
+                variations: [
+                    {
+                        name: "",
+                        nutrition: {
+                            calories: 13.3,
+                            fat: 0,
+                            sugar: 0,
+                            protein: 1.4,
+                            caffeine: 200
+                        }
+                    }
+                ]
+            },
+            {
+                name: "Triple",
+                volume: 3,
+                variations: [
+                    {
+                        name: "",
+                        nutrition: {
+                            calories: 19.6,
+                            fat: 0,
+                            sugar: 0,
+                            protein: 2.3,
+                            caffeine: 293.5
+                        }
+                    }
+                ]
+            },
+            {
+                name: "Quad",
+                volume: 4,
+                variations: [
+                    {
+                        name: "",
+                        nutrition: {
+                            calories: 26.7,
+                            fat: 0,
+                            sugar: 0,
+                            protein: 3.2,
+                            caffeine: 400
+                        }
+                    }
+                ]
+            }
+    
+        ]
+    }
 
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
-	}
-
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
-
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
-
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
-
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
+    render() {
 		return (
-			<div class={style.detail}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
-
-				<div>Current time: {new Date(time).toLocaleString()}</div>
-
-				<p>
-					<button onClick={this.increment}>Click Me</button>
-					{' '}
-					Clicked {count} times.
-				</p>
+			<div class={style.header}>
+				<h1>Detail: {this.state.name}</h1>
+                <img src={this.state.image} />
 			</div>
 		);
 	}
