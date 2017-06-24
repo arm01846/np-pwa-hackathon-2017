15a103e9483345e81e744b94a208d135a6d16892
Sukrit Amornpornwiwat <arm01846@gmail.com>
2017-06-24 19:51:01 +0700

    styling list

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -10,12 +10,12 @@ export default class MenuItem extends Component {
     render() {
         return (
             <Link href={'/detail/' + this.props.link}>
-            <div style={{ width: '100%', height: '100px', display: 'flex', margin: '8px' }}>
+            <div style={{ width: '100%', height: '100px', display: 'flex', margin: '8px', 'background-color': '#007143' }}>
                 <div>
                     <img style={{ width: '100px', height: '100px' }} src={ this.props.image }/>
                 </div>
                 <div>
-                    <div style={{ height: '40px', lineHeight: '40px' }} class={style.displayContent}>{ this.props.name }</div>
+                    <div style={{ height: '40px', lineHeight: '40px', 'font-size': '1.25em' }} class={style.displayContent}>{ this.props.name }</div>
                     <Link href={'/detail/' + this.props.link + '/tall'}>
                         <div style={{ height: '60px', lineHeight: '60px' }} class={style.displayContent}>line 2</div>
                     </Link>
