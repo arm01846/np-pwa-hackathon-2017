d62b5c41afda1fd9f453283cc02ac7114a8065cb
Sukrit Amornpornwiwat <arm01846@gmail.com>
2017-06-24 20:50:24 +0700

    styling menu component

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -1,6 +1,8 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
+import mdl from 'material-design-lite/material';
+import { Chip } from 'preact-mdl';
 
 export default class MenuItem extends Component {
     constructor(props) {
@@ -15,10 +17,16 @@ export default class MenuItem extends Component {
                     <img style={{ width: '100px', height: '100px' }} src={ this.props.image }/>
                 </div>
                 <div>
-                    <div style={{ height: '40px', lineHeight: '40px', 'font-size': '1.25em' }} class={style.displayContent}>{ this.props.name }</div>
-                    <Link href={'/detail/' + this.props.link + '/tall'}>
-                        <div style={{ height: '60px', lineHeight: '60px' }} class={style.displayContent}>line 2</div>
-                    </Link>
+                    <div style={{ height: '40px', lineHeight: '40px', 'font-size': '1.25em', color: '#007143' }} class={style.displayContent}>{ this.props.name }</div>
+                    
+                    <div style={{ height: '60px', lineHeight: '60px' }} class={style.displayContent}>
+                        { this.props.size.map((size) => {
+                            return <Link href={'/detail/' + this.props.link + '/' + size.name}
+                                style={{ padding: '5px 12px', margin: '0px 8px', color: '#fefefe', 'background-color': '#007143' }}
+                            ><strong>{size.name}</strong></Link>
+                        })}
+                    </div>
+                    
                 </div>
             </div>  
             </Link>
--- src/routes/menu/index.js
+++ src/routes/menu/index.js
@@ -1,7 +1,7 @@
 import { h, Component } from 'preact';
 import style from './style';
 import mdl from 'material-design-lite/material';
-import { List, ListItem, ListItemContent } from 'preact-mdl';
+import { List, ListItem, ListItemContent, Card, CardTitle } from 'preact-mdl';
 import MenuItem from '../../components/menu-item';
 
 import { menu } from './menu';
@@ -10,14 +10,17 @@ export default class Menu extends Component {
 	render() {
 		return (
 			<div class={style.menu}>
-				<List>
-					{ menu.menu.map((coffee) => {
-						return <MenuItem 
-							name={coffee.name} 
-							link={coffee.link}
-							image={coffee.image}></MenuItem>
-					}) }
-				</List>
+				{ menu.menu.map((coffee) => {
+					return <Card shadow={4} style={{ width: 'auto', height: '100px', 'min-height': '100px', 'margin-bottom': '16px' }}>
+						<CardTitle expand style={{ padding: '0' }}>
+						<MenuItem 
+						name={coffee.name} 
+						link={coffee.link}
+						image={coffee.image}
+						size={coffee.size}></MenuItem>
+						</CardTitle>
+						</Card>
+				}) }
 			</div>
 		);
 	}
