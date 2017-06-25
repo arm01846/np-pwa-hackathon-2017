4abd74322149430c2b1ad16489b1780cc67b72ef
Natkrita Chumlek <sariakra@gmail.com>
2017-06-25 17:57:30 +0700

    add link to home in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,20 @@
 import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
 import style from './style';
 
 export default class Header extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
+		console.log(location);
 		return (
 			<header class={style.header} style={{ 'text-align': 'center' }}>
-				<h1 >Coffee List</h1>
+				<Link href="/">
+					<h1>Coffee List</h1>
+				</Link>
+				
 			</header>
 		);
 	}
